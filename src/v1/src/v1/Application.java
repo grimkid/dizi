630e93550f99f2750b1df71d7fbdc8fc227a3b93
Rares <gafton.rares@gmail.com>
2018-05-29 00:14:51 +0300

    initial commit

---
+++ src/v1/src/v1/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package v1;
+import org.eclipse.milo.opcua.stack.client;
+/**
+ * @author Rares
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
